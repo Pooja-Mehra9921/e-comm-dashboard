28537dbfb0a9bcba54df34439d4a1c2760e88b79
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2025-04-13 21:28:41 +0530

    making frontent and backend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import Footer from './component/Footer';
+import Header from './component/Header';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Signup from './component/Signup';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+ <Header/>
+<Routes>
+  <Route path='/' element={<h1>this is a home page</h1>}/>
+  <Route path='/product' element={<h1>this is a product page</h1>}/>
+  <Route path='/add' element={<h1>this is a add page</h1>}/>
+  <Route path='/update' element={<h1>this is a update page</h1>}/>
+  <Route path='/logout' element={<h1>this is a logout page</h1>}/>
+  <Route path='/profile' element={<h1>this is a profile page</h1>}/>
+  <Route path='/signup' element={<Signup/>}/>
+</Routes>
+<Footer/>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
