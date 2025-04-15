25bcf3043ac80377ff870678f9fc3b3c341da9d8
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2025-04-15 22:50:07 +0530

    making add product ui

--- frontend/src/component/AddProduct/index.jsx
+++ frontend/src/component/AddProduct/index.jsx
@@ -5,12 +5,14 @@ const AddProduct = ()=>{
     return(
         <>
         <div className="add-prod-main-container">
-        <h1>Add Product</h1>
-        <input type="text" placeholder="Enter product name" />
-        <input type="text" placeholder="Enter product price" />
-        <input type="text" placeholder="Enter product category" />
-        <input type="text" placeholder="Enter product company" />
-        <button>Add Product</button>
+        <div className="add-prod-container">
+        <h1 className="product-heading">Add Product</h1>
+        <input className="inputbox" type="text" placeholder="Enter product name" />
+        <input className="inputbox" type="text" placeholder="Enter product price" />
+        <input className="inputbox" type="text" placeholder="Enter product category" />
+        <input className="inputbox" type="text" placeholder="Enter product company" />
+        <button className="add-product-btn">Add Product</button>
+        </div>
         </div>
 
 
